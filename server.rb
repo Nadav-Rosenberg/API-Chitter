35f1b0834ed06e6c9ebcbf27aacbd0a6b3776195
Nadav Rosenberggi <nadav.rosenberg@gmail.com>
2015-04-20 14:09:05 +0100

    adds first test for posting a peep into the database

--- server.rb
+++ server.rb
@@ -7,4 +7,9 @@ class APIChitter < Sinatra::Base
   get '/' do
     'hello chitter api'
   end
+
+  post '/api/new_peep' do
+    peep = params[:peep]
+    Peep.create(content: peep)
+  end  
 end
---
+++ spec/spec_helper.rb
@@ -0,0 +1,33 @@
+ENV['RACK_ENV'] = 'test'
+
+# require 'coveralls'
+# require 'simplecov'
+require 'capybara/rspec'
+require 'database_cleaner'
+
+require './server'
+
+Capybara.app = APIChitter
+
+# SimpleCov.formatters = [
+#   SimpleCov::Formatter::HTMLFormatter,
+#   Coveralls::SimpleCov::Formatter
+# ]
+# Coveralls.wear!
+
+# RSpec.configure do |config|
+
+#   config.before(:suite) do
+#     DatabaseCleaner.strategy = :transaction
+#     DatabaseCleaner.clean_with(:truncation)
+#   end
+
+#   config.before(:each) do
+#     DatabaseCleaner.start
+#   end
+
+#   config.after(:each) do
+#     DatabaseCleaner.clean
+#   end
+
+# end
