25c8346849c2b0232632a52b80696a160590e78a
Nadav Rosenberggi <nadav.rosenberg@gmail.com>
2015-04-22 19:54:31 +0100

    fixes rspec bugs related to JSON

--- helpers.rb
+++ helpers.rb
@@ -19,33 +19,22 @@ module Helpers
   end
 
   def sign_in
-    p params
-    p params["email"]
     email, password = params["email"], params["password"]
     user = User.authenticate(email, password) 
       if user
-        puts "Success"
         session[:user_id] = user.id 
-        p session[:user_id]
         message = {message: "Welcome, #{user.user_name}!"}
         JSON.generate(message)
       else  
         "user details are incorrect" 
-      end
-
-    p session[:user_id]
-    ""  
+      end 
   end
 
   def sign_out
-    puts "wtf"
     session[:user_id] = nil
   end
 
   def new_peep
-    puts 'here'
-    p params
-    p session[:user_id]
     if session[:user_id]
       peep = params["peep"]
       Peep.create(content: peep, time: Time.now.to_s[0..18], author: User.first(id: session[:user_id]).user_name)
@@ -60,7 +49,7 @@ module Helpers
     Peep.all.each do |peep|
       peeps << {text: peep.content, author: peep.author, time: peep.time}
     end
-    p JSON.generate(peeps)
+    JSON.generate(peeps)
   end  
 
 end  
